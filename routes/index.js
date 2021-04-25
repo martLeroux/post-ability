1556ca08644c9b48c87d49f9894cd72bce46f4f6
Leroux Martin <tintin_151@hotmail.com>
2021-04-25 19:35:36 -0400

    header

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,10 @@ var md = require('jstransformer')(require('jstransformer-markdown-it'));
 /* GET home page. */
 router.get('/', (req, res) => {
   indexContent.getIndex().then(function (data) {
-    console.log(data.items[0].fields);
     const aboutDescription = data.items[0].fields.aboutDescription;
     const whoAmIDescription = data.items[0].fields.whoAmIDescription;
     res.render('index', {
-      title: 'Express',
+      title: 'Post-Ability',
       data: data.items[0].fields,
       aboutDescription: md.render(aboutDescription).body,
       whoAmIDescription: md.render(whoAmIDescription).body,
