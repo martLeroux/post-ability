86a069a1f2b65d405046920879a50d29fc9ab7b5
Leroux Martin <tintin_151@hotmail.com>
2021-05-18 21:34:58 -0400

    test redirect https

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.engine('handlebars', exphbs({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
+app.use((request, res, next) => {
+    if (app.get('env') !== 'development' && !request.secure) {
+        return response.redirect("https://" + request.headers.host + request.url);
+    }
+
+    next();
+})
+
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -46,7 +54,6 @@ app.use('/users', users);
 
 /// catch 404 and forward to error handler
 app.use((req, res, next) => {
-
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -67,13 +74,9 @@ if (app.get('env') === 'development') {
     });
 }
 
-
-
-app.enable('trust proxy')
 // production error handler
 // no stacktraces leaked to user
 app.use((err, req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
